7ac5cee57451422a0220f0bd87a785455cda713b
L <L@789.com>
2022-02-17 12:07:53 +0800

    Initial commit

---
+++ src/main/java/com/lx/Boot01Helloworld2Application.java
@@ -0,0 +1,16 @@
+package com.lx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot01Helloworld2Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Boot01Helloworld2Application.class, args);
+
+        //1233211234567
+    }
+
+}
