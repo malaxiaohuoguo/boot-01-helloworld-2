d38d08f473008110505dfa2409c6994d1cf8580d
L <L@789.com>
2022-02-17 13:32:32 +0800

    hot-fix first commit

--- src/main/java/com/lx/Boot01Helloworld2Application.java
+++ src/main/java/com/lx/Boot01Helloworld2Application.java
@@ -13,6 +13,7 @@ public class Boot01Helloworld2Application {
         //1233211234567
         System.out.println("dhsk");
         System.out.println("bdfhe");
+        System.out.println("hot-fix djs");
     }
 
 }
